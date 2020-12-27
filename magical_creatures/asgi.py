1f4a7a0d08b453e332d544c3efb0e2a4aaf51bee
Adam Hill <ahill@fool.com>
2020-12-27 09:24:55 -0500

    Initial commit of typical Django app created with `startproject` with a barebones `www` app to render an index page.

---
+++ magical_creatures/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for magical_creatures project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magical_creatures.settings')
+
+application = get_asgi_application()
---
+++ magical_creatures/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for magical_creatures project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magical_creatures.settings')
+
+application = get_wsgi_application()
