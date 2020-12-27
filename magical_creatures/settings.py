b9d27bb0c952427e87e9f23c226b300023cee44f
Adam Hill <ahill@fool.com>
2020-12-27 09:28:38 -0500

    Create first Unicorn component named `sightings`. Add `unicorn` app to Django.

--- magical_creatures/settings.py
+++ magical_creatures/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "www",
     "django_unicorn",
+    "unicorn",
 ]
 
 MIDDLEWARE = [
---
+++ unicorn/components/sightings.py
@@ -0,0 +1,5 @@
+from django_unicorn.components import UnicornView
+
+
+class SightingsView(UnicornView):
+    count = 0
